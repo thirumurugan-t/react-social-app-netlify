de0d4bf0eb93812e0fac426a2f3fce4dc02c80ec
thirumurugan-t <thirumurugant536@gmail.com>
2025-05-25 15:14:09 +0530

    files have been updated

--- src/Socialmedia app/UploadPost.js
+++ src/Socialmedia app/UploadPost.js
@@ -5,12 +5,15 @@ import { Link } from "react-router-dom";
 const UploadPost = ({ post }) => {
   return (
     <div className="post-container">
+      <div style={{marginBottom:"15px"}}>
       <Link to={`/newpost/${post.id}`} style={{ textDecoration: "none" }}>
         <h1 className="post-title">{post.title}</h1>
         <p className="post-date">{post.datetime}</p>
       </Link>
 
       <p className="post-body">{post.body}</p>
+      </div>
+     
     </div>
   );
 };
